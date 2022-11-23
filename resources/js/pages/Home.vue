5ec4990058c2fc9f88f284f36f2bfeaffb8319c0
Mustafa al-nashie <44943708+Musti132@users.noreply.github.com>
2022-11-23 12:45:48 +0100

    Add home page text

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="container">
-        <h1>My App</h1>
-        <p>My first paragraph.</p>
-        <v-btn color="primary">My Button</v-btn>
-    </div>
+    <div class="text-h2 font-weight-bold">Welcome to IntelRobotics</div>
+    <p class="text-left text-wrap" style="width: 80rem; display: inline-block;">
+        <v-spacer></v-spacer><p>IntelRobotics is a firm that specializes in the
+        design and manufacture of robots. We have a wide range of products that
+        are suitable for both personal and commercial use. Our robots are
+        designed to make your life easier and help you achieve your goals. We
+        are constantly innovating and expanding our range of products to meet
+        the needs of our customers. We have a team of dedicated and
+        knowledgeable staff who are always on hand to help you choose the right
+        product for your needs. </p><v-spacer></v-spacer><p>
+        <v-spacer></v-spacer></p>
+
+        <v-btn color="primary" class="mr-4" to="/products">Products</v-btn>
+
+    </p>
 </template>
 
 <script>
